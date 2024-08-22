56919dedd53f07aeab3bde29f10121c9d0696246
Didi <ddgrisalesg@uqvirtual.edu.co>
2024-08-22 18:06:46 -0500

    Firt Commit
    firts commit repo

---
+++ Python/UnionConjuntos.py
@@ -0,0 +1,10 @@
+#Conjuntos para realizar la unión
+conjuntoA = {"A", "B" , "C", "D", "E"}
+conjuntoB = {"B", "G", "G", "J"}
+conjuntoC = {"Z", "D", "X"}
+
+#Unión de los conjuntos
+unionABC =  conjuntoA.union(conjuntoB, conjuntoC)
+
+#Vemos la unión de conjuntos
+print(unionABC)
