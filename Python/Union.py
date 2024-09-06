4e8fa8c973dfe70acb2c51b04e7e05f8475c255a
Didi <ddgrisalesg@uqvirtual.edu.co>
2024-09-06 17:06:23 -0500

    Commit
    union

--- Python/Union.py
+++ Python/Union.py
@@ -1,8 +1,12 @@
 import matplotlib.pyplot as plt
 from matplotlib_venn_wordcloud import venn3_wordcloud
-
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
+
+#Conjuntos                  
+conjuntoA = {"A", "B", "C", "D", "E"}
+conjuntoB = {"B", "G", "J"} 
+conjuntoC = {"Z", "D", "X","B"}
 
 #Funcion para realizar la Union entre conjuntos
 def union_conjuntos(*conjuntos):
@@ -14,10 +18,6 @@ def union_conjuntos(*conjuntos):
             resultado.add(elem)
     return resultado
 
-#Conjuntos
-conjuntoA = {"A", "B", "C", "D", "E"}
-conjuntoB = {"B", "G", "J"} 
-conjuntoC = {"Z", "D", "X","B"}
 
 #Realizamos el resultado de la union
 unionABC = union_conjuntos(conjuntoA, conjuntoB, conjuntoC)
@@ -29,17 +29,24 @@ venn = venn3_wordcloud([conjuntoA, conjuntoB, conjuntoC],
                        ('Conjunto A', 'Conjunto B', 'conjunto C'))
 plt.show()
 
-#Creamos una visual de lso elementos de la Union
-
 #Creamos una cadena de texto vacia para litar los elementos de la Union
-union_txt = ' '.join(unionABC)
+union_txt = ''.join(unionABC)
 
-WordCloud = WordCloud(width=200, height=150, background_color='white', max_font_size=30).generate(union_txt)
+wordcloud = WordCloud(
+    width=500,                    
+    height=250,                   
+    background_color='white',     
+    colormap='rainbow',           
+    contour_color='white',        
+    contour_width=2               
+).generate(union_txt)
 
 # Mostrar la nube de palabras
 plt.figure(figsize=(10, 5))
-plt.imshow(WordCloud, interpolation='bilinear')
-plt.axis('off')
-plt.title("Visualización del Conjunto Unión")
-
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis("off")  # Ocultar los ejes
 plt.show()
+
+
+
+
