b89b59b2c1484c8e5798b501f8a3126660453a90
Diego Nicolás Peña Rincon; Escritorio. <diegonicolaspenarincon@gmail.com>
2024-09-06 21:39:28 -0500

    Se modifico el metodo para el complemento del subconjunto

--- Python/SuperConjuntos.py
+++ Python/SuperConjuntos.py
@@ -2,20 +2,20 @@
 , tener en cuenta que la funcion devuelve un conjunto con los elementos que le faltan a A para ser igual que el universal"""
 
 def definirConjuntComplemento(conjuntoUniversal,conjuntoA):
-    conjuntoComplemento=[]
+    conjuntoComplemento=set()
     contador=0
     for elementoUniversal in conjuntoUniversal:
         for elementoA in conjuntoA:
             if elementoUniversal==elementoA:
                 contador+=1
         if contador==0:
-            conjuntoComplemento.append(elementoUniversal)
+            conjuntoComplemento.add(elementoUniversal)
         contador=0
     return conjuntoComplemento
 
 """Instancia el conjunto universal y el A. Creo una variable donde guardar el conjunto resultante y
 envio por parametro los dos conjuntos al metodo definirConjuntComplemento """
-conjuntoUniversal=["no","quiero","hacer","tareas","especialmente","las de tlf"]
-conjuntoA=["no","las de tlf","hacer","tareas","quiero irme a dormir"]
+conjuntoUniversal={'A','B','C','D','E','F'}
+conjuntoA={'A','E'}
 conjuntoComplemento=definirConjuntComplemento(conjuntoUniversal,conjuntoA)
 print(conjuntoComplemento)
